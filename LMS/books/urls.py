36ae49e63a921d977f16b06a8aa62f5e17ec0d32
Omar Assal <117770253+OmarAdelAssal@users.noreply.github.com>
2023-08-19 02:15:22 +0200

    done

--- LMS/books/urls.py
+++ LMS/books/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from books.views import index
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('', index, name='books.index'),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- LMS/books/views.py
+++ LMS/books/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from books.models import Book
 # Create your views here.
 def index(request):
-    return render(request, "books/index.html")
+    books = Book.get_all_books()
+    return render( request, "books/index.html", context={'books': books} )
