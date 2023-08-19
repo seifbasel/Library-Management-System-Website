2563d49e14acd88356b161f3af7cf296e23ba581
seif basel <seifbasel950@gmail.com>
2023-08-19 16:07:18 +0300

    done

--- LMS/books/views.py
+++ LMS/books/views.py
@@ -4,3 +4,9 @@ from books.models import Book
 def index(request):
     books = Book.get_all_books()
     return render( request, "books/index.html", context={'books': books} )
+
+
+def show_book(request):
+    # books = Book.get_all_books()
+    books = Book.objects.get(id=5)
+    return render( request, "books/show.html", context={'books': books})
