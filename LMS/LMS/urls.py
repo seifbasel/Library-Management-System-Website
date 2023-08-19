178e20d1de27d1f400d0c2d0c0e1b3371889c104
Omar Assal <117770253+OmarAdelAssal@users.noreply.github.com>
2023-08-19 13:24:37 +0200

    Genre

--- LMS/LMS/urls.py
+++ LMS/LMS/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("students/", include('students.urls')),
     path("books/", include('books.urls')),
+    path("genre/", include('genre.urls')),
 ]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
---
+++ LMS/genre/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+# from genre.views import ''
+
+urlpatterns = [
+
+]
