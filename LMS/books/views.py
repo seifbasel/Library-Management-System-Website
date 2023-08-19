fa88f05856e4b2bafa65e7707beb888f2ff9ac7e
seif basel <seifbasel950@gmail.com>
2023-08-19 22:12:53 +0300

    done

--- LMS/books/views.py
+++ LMS/books/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import get_object_or_404, render,redirect
 from books.models import Book
 # Create your views here.
 def index(request):
@@ -10,24 +10,31 @@ def show(request, id):
     book = Book.get_book(id=id)
     return render( request, "books/show.html", context={'book': book})
 
-def book_creat_view(request):
-    book = Book.get_all_books()
-    if request.method == "POST":
-        title = request.POST['title']
-        author = request.POST['author']
-        price = request.POST['price']
-        description = request.POST['description']
-        image= request.POST['image']
+
+
+
+# def book_creat_view(request):
+#     book = Book.get_all_books()
+#     if request.method == "POST":
+#         title = request.POST['title']
+#         author = request.POST['author']
+#         price = request.POST['price']
+#         description = request.POST['description']
+#         image= request.POST['image']
     
-        book=Book()
-        book.title=title
-        book.author = author
-        book.price = price
-        book.description = description
-        book.image=image
-        book.save()
+#         book=Book()
+#         book.title=title
+#         book.author = author
+#         book.price = price
+#         book.description = description
+#         book.image=image
+#         book.save()
         
 
-        return redirect('books.index')
-    return render(request,'books/creat.html',context={'book':book})
+#         return redirect('books.index')
+#     return render(request,'books/creat.html',context={'book':book})
 
+# def delete(request, id):
+#     book = get_object_or_404(Book, id=id)  # object from student model
+#     book.delete()  # delete object from the database
+#     return redirect('books.index')
