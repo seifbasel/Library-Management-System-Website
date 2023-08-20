f49927c80804e83e04c298f565f81e84e698875c
Omar Assal <117770253+OmarAdelAssal@users.noreply.github.com>
2023-08-20 21:24:24 +0200

    signup using class based view

--- LMS/students/urls.py
+++ LMS/students/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from students.views import index,login,signup,student_creat_view,profile
+from students.views import index,login, profile, StudentSignUp
 
 urlpatterns = [
     path('', index, name='student.index'),
     path('login/', login, name='student.login'),
     path('profile/', profile, name='student.profile'),
-    path('signup/', student_creat_view,name='student.signup'),
+    path('signup/', StudentSignUp.as_view(),name='student.signup'),
     
 ]
--- LMS/students/views.py
+++ LMS/students/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render,redirect
-from .forms import Student_form
-from .models import Student
-# Create your views here.
+from students.forms import Student_form
+from students.models import Student
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.views.generic import CreateView
 def index(request):
     return render(request, "students/index.html")
 
@@ -21,8 +20,8 @@ def login(request):
     return render(request, "students/login.html")
 
 
-def signup(request):
-    return render(request, "students/signup.html")
+# def signup(request):
+#     return render(request, "students/signup.html")
 
 def student_creat_view(request):
     # form=Student_form(request.POST or None)
@@ -54,3 +53,9 @@ def student_creat_view(request):
 
         return redirect('student.index')
     return render(request,'students/signup.html',context={'student':student})
+
+class StudentSignUp(CreateView):
+    model = Student
+    form_class = Student_form
+    success_url = "/students/login"
+    template_name = "students/signup.html"
