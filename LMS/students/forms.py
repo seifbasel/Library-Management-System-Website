47e95b0062da76a8b933ad441a3211644ff43039
Omar Assal <117770253+OmarAdelAssal@users.noreply.github.com>
2023-08-20 16:43:19 +0200

    12

--- LMS/students/forms.py
+++ LMS/students/forms.py
@@ -1,24 +1,7 @@
 from django import forms
-from students.models import Student
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from .models import Student
 
 
-class SignupForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'password1', 'password2')
-
-class LoginForm(forms.Form):
-    username = forms.CharField(max_length=255)
-    password = forms.CharField(widget=forms.PasswordInput)
-
-
-
-'''
-old code 
 class Student_form(forms.ModelForm):
     class Meta:
         model=Student
@@ -30,5 +13,4 @@ class Student_form(forms.ModelForm):
             'birthdate',
             
         ]
-
-'''
+        
--- LMS/students/urls.py
+++ LMS/students/urls.py
@@ -1,13 +1,10 @@
-from django.urls import path,include
-from students.views import index,login,profile,signup_view, login_view#,signup,student_creat_view
+from django.urls import path
+from students.views import index,login,signup,student_creat_view,profile
 
 urlpatterns = [
     path('', index, name='student.index'),
     path('login/', login, name='student.login'),
     path('profile/', profile, name='student.profile'),
-    path('signup/', signup_view, name='students.signup'),
-    path('login/', login_view, name='students.login'),
-    
-
+    path('signup/', student_creat_view,name='student.signup'),
     
 ]
