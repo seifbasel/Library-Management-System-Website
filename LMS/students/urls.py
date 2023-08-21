4b78b2364c7705f21fb63d4398a22e7dfc94d0c7
Omar Assal <117770253+OmarAdelAssal@users.noreply.github.com>
2023-08-21 21:43:51 +0200

    1

--- LMS/students/urls.py
+++ LMS/students/urls.py
@@ -10,10 +10,7 @@ urlpatterns = [
     path('index/', index, name='student.index'),
     path('login/', login, name='student.login'),
     path('profile/', profile, name='student.profile'),
-<<<<<<< HEAD
-=======
     path('signup/', StudentSignUp.as_view(),name='student.signup'),
     
->>>>>>> e0e491146f8cd6d70c30aa99494cb73e906be496
     # path('signup/', student_creat_view,name='student.signup'),
 ]
