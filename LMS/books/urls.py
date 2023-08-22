3a3e4b72ab0b014793a972b6ae039577848be723
Omar Assal <117770253+OmarAdelAssal@users.noreply.github.com>
2023-08-22 17:25:09 +0200

    borrow

--- LMS/books/urls.py
+++ LMS/books/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from books.views import index,show
+from books.views import index,show,borrow
 
 from django.conf.urls.static import static
 from django.conf import settings
@@ -7,5 +7,6 @@ from django.conf import settings
 urlpatterns = [
     path('', index, name='books.index'),
     path("<int:id>", show, name="books.show"),
+    path("books/<int:id>", borrow, name="books.borrow")
     # path("creat", book_creat_view, name="books.creat"),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- LMS/books/views.py
+++ LMS/books/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import get_object_or_404, render,redirect
+from django.http import HttpResponse
 from books.models import Book
+from status.models import Status
+
 # Create your views here.
 def index(request):
     books = Book.get_all_books()
@@ -10,3 +13,10 @@ def show(request, id):
     book = Book.get_book(id=id)
     return render( request, "books/show.html", context={'book': book})
 
+def borrow(request, id):
+    book = Book.get_book(id=id)
+    status = Status.objects.get(name='Borrowed')
+    book.status = status
+    book.save()
+    return HttpResponse("<h1>Borrowed</h1>")
+    
