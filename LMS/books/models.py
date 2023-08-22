455512514056e0f76fea965e66927d31eb0e4a44
Omar Assal <117770253+OmarAdelAssal@users.noreply.github.com>
2023-08-22 23:50:45 +0200

    push

--- LMS/books/models.py
+++ LMS/books/models.py
@@ -4,6 +4,7 @@ from status.models import Status
 from django.urls import reverse
 from django.contrib.auth.models import User
 # Create your models here.
+#hello
 class Book(models.Model):
 
     title = models.CharField(max_length=255)
