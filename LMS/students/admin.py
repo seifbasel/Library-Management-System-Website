56dd7e5a785993149ca9b05eec050f99d9943997
Omar Assal <117770253+OmarAdelAssal@users.noreply.github.com>
2023-08-23 15:54:24 +0200

    1

--- LMS/students/admin.py
+++ LMS/students/admin.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from students.models import Student
 
-class MyModelAdmin(admin.ModelAdmin):
+class StudentID(admin.ModelAdmin):
     readonly_fields = ('id',)
 
 
 
 
-admin.site.register(Student,MyModelAdmin)
+admin.site.register(Student,StudentID)
