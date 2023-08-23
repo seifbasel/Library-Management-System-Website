16491603f58f788a74a8b3b3492ffd9e772289e2
Omar Assal <117770253+OmarAdelAssal@users.noreply.github.com>
2023-08-24 00:33:28 +0200

    2

--- LMS/students/models.py
+++ LMS/students/models.py
@@ -7,7 +7,7 @@ from books.models import Book
 
 class Student(models.Model):
     name = models.CharField(max_length=255)
-    password = models.CharField(max_length=255, default=1234)
+    # password = models.CharField(max_length=255, default=1234)
     phone_number = models.IntegerField(null=True, blank=True)
     email = models.EmailField(null=True, blank=True)
     birthdate = models.DateField(max_length=8, null=True, blank=True)
