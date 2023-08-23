4eb34598f41b8f08779b63e1938b9be234413991
Omar Assal <117770253+OmarAdelAssal@users.noreply.github.com>
2023-08-23 23:08:48 +0200

    done done done

--- LMS/books/views.py
+++ LMS/books/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import get_object_or_404, render,redirect
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from books.models import Book
-from books.forms import BorrowModelForm
 from status.models import Status
-from students.models import Student
+
 
 # Create your views here.
 def index(request):
@@ -17,22 +16,29 @@ def show(request, id):
     book = Book.get_book(id=id)
     return render( request, "books/show.html", context={'book': book})
 
+## Borrow Function
+from datetime import date, timedelta
 def borrow(request, id):
     book = Book.get_book(id=id)
-    form = BorrowModelForm(instance=book)
-    if request.POST:
-        form = BorrowModelForm(request.POST, request.FILES, instance=book)
-        if form.is_valid():
+    if request.method == "POST":
             status = Status.objects.get(name='Borrowed')
             book.status = status
             book.user = request.user
-            book.save()
-            form.save()
-            # return HttpResponse("<h1>Borrowed</h1>")
-            return redirect("books.borrowed")
+            borrow_period = int(request.POST['borrow_period'])
+            borrow_date = date.today()
+            return_date = borrow_date + timedelta(days=borrow_period)
 
-    return render(request , "books/borrowed.html", context={"form":form})
-    
+            book.borrow_period = borrow_period
+            book.borrow_date = borrow_date
+            book.return_date = return_date
+            book.save()
+            return redirect("books.borrowed")
+    return render(request , "books/borrowed.html")
+            
+
+
+
+
 def show_borrowed(request):
     books = Book.objects.filter(user=request.user)
     return render(request , "books/borrowedbooks.html", context={"books":books})
